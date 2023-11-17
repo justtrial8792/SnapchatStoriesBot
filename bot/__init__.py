fa74f72f2e4034b7185d26447e3b64bbe19d6799
justtrial8792 <149057676+justtrial8792@users.noreply.github.com>
2023-11-17 11:50:40 +0600

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -10,10 +10,10 @@ logging.getLogger("urllib3").setLevel(logging.WARNING)
 
 
 class ENV_VARS(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME")
+    API_ID = int(os.environ.get("API_ID", "25222859"))
+    API_HASH = os.environ.get("API_HASH", "b76d4fb1f5afc4a797b85c5e34b0f1ca")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6458225530:AAFhj-q7M-EdXi9UcB2r7tcq6vCTVSrE2cQ")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Url_to_stream_bot")
     #AUTH_USER = int(os.environ.get("AUTH_USER", 5071059420))
 
 
